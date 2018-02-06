fc255aee96f2549c58e61c1007106059074a33eb
User <User@FULL308-WIN.FULL.Creative>
2018-02-06 12:29:38 +0530

    ytu

--- sample/src/packge/NewClass.java
+++ sample/src/packge/NewClass.java
@@ -6,7 +6,7 @@ public class NewClass {
 		// TODO Auto-generated method stub
         System.out.println("This is second class");
         System.out.println("This is second class");
-        
+        System.out.println("This is second class");
 	}
 
 }
