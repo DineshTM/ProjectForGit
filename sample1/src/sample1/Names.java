c3f2048aba0cec4bd5fcaf818e951c135fbae94c
User <User@FULL308-WIN.FULL.Creative>
2018-02-06 12:21:52 +0530

    This is for commiting the new class in sample

--- sample1/src/sample1/Names.java
+++ sample1/src/sample1/Names.java
@@ -4,7 +4,8 @@ public class Names {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		final int NUMBER=7;
+        System.out.println("This was naming class");
 	}
 
 }
